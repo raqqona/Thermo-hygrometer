8906aa6d6decdd86a20dba5999001da807f4e23f
raqqona <e1n18047@st.oit.ac.jp>
2022-07-29 11:59:59 +0900

    change slacker to bolt

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from slack.bot import Bot
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-def main():
-    bot = Bot()
-    bot.run()
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+
+@app.message("何度")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
 
 if __name__ == "__main__":
-    main()
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
--- slackbot_settings.py
+++
@@ -1,5 +0,0 @@
-APT_TOKEN = "***********"
-
-DEFAULT_REPLAY = "ちゃんと確認して送信しろカス"
-
-PLUGINS = ['plugins']
